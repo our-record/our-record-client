7b600450e13e1e1c4c83dffc03b1483f95d1fda5
shinmiyoung <smy910102@gmail.com>
2021-07-20 22:43:10 +0900

    Add: register 레아이웃

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import Main from './pages/Main/Main';
+import Register from './pages/Register/Register';
 import TestForm from './pages/Test/TestForm';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
@@ -13,6 +14,7 @@ const Routes = () => (
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Main} />
+          <Route exact path="/register" component={Register} />
           <Route exact path="/test" component={TestForm} />
         </Switch>
       </BrowserRouter>
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,7 +1,7 @@
 const theme = {
   fontFamily: "'Noto Sans KR', sans-serif;",
   basicBorder: '1px solid rgb(220, 220, 220)',
-  keyColor: '#FDD3B9',
+  keyColor: '#fdd3b9',
   basicDarkGray: 'rgb(51, 51, 51)',
   basicGray: 'rgb(120, 120, 120)',
 };
